b5dc65748ed59d3bb71e69aba3b8d35c75143a04
Sai Khant Zay Lynn Yaung <khantzay.ly@gmail.com>
2023-03-06 17:19:59 +0630

    second update

--- main.py
+++ main.py
@@ -1,33 +1,83 @@
-from flask import Flask
-from flask_restful import Api , Resource
+from flask import Flask, jsonify, request,g
+from flask_restful import Api, Resource
+import jwt
+import datetime
+from functools import wraps
+from pymongo import MongoClient
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 api = Api(app)
+CORS(app)
+app.config['SECRET_KEY'] = "andrew"
 
-class HellWorld(Resource):
-    def get(self,name,test):
-        return {"data" : name ,"test":test}
+cluster = MongoClient("mongodb+srv://aviothicedu:hello@cluster0.nghuy2n.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["Data"]
+collection = db["user_data"]
 
-    def post(self):
-        return {"data" : "Hello Wor"}
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
 
-class add(Resource):
-    def get(self,onenumber,twonumber):
-        threenumber = onenumber + twonumber
-        return { "data" : threenumber}
+        if not token:
+            return {'message': 'Token is missing'}, 401
+        
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            g.user = data['user']
+
+        except:
+            return {'message': 'Token is invalid'}, 401
+
+        return f(*args, **kwargs)
+
+    return decorated
 
 
-class minus(Resource):
-    def get(self,onenumber,twonumber):
-        threenumber = onenumber - twonumber
-        return {"data": threenumber}
+class UnprotectedResource(Resource):
+    def get(self):
+        return jsonify({'message': 'Anyone can view this!'})
+
+class ProtectedResource(Resource):
+    @token_required
+    def get(self):
+        results = collection.find_one({"username":g.user})
+        if results:
+            results.pop('_id', None)
+            return jsonify(results)
+
+class signupResource(Resource):
+    def get(self,username,password):
+        
+        result = collection.find_one({"username":username})
+        if result:
+            return {'message': 'Username already exists'}, 409
+        
+        post = {"username":username,"password":password}
+        collection.insert_one(post)
+        return {'message': 'signup sucessfully'},201
+        
+
+class LoginResource(Resource):
+    def get(self, username, password):
+
+        results = collection.find_one({"username":username})
+        if results:
+            if password == results["password"]:
+                token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                                app.config['SECRET_KEY'], algorithm='HS256')
+                return {'token': token}
+            else:
+                return jsonify({'message':'Wrong Password'}),401
+
+        return jsonify({'message':'Username not found'}),401
 
 
-api.add_resource(HellWorld,"/helloworld/<string:name>/<int:test>")
-api.add_resource(add,"/add/<int:onenumber>/<int:twonumber>")
-api.add_resource(minus,"/minus/<int:onenumber>/<int:twonumber>")
+api.add_resource(LoginResource, '/login/<string:username>/<string:password>')
+api.add_resource(UnprotectedResource, '/unprotected')
+api.add_resource(ProtectedResource, '/protected')
+api.add_resource(signupResource,'/signup/<string:username>/<string:password>')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
